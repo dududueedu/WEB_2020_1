91743e01e3bf275d7de1623e13e732763a183663
Eduardo da Silva Gomes <eduardooffside@gmail.com>
2020-08-16 18:53:04 -0300

    atividade4

--- atividade3/crud/src/components/List.jsx
+++ atividade3/crud/src/components/List.jsx
@@ -1,10 +1,59 @@
 import React, { Component } from 'react'
+import Axios from 'axios'
+import TableRow from './TableRow'
 
 export default class List extends Component{
+    
+    constructor(props) {
+        super(props)
+        this.state = { disciplinas: [] }
+        //this.deleteElementId = this.deleteElementId.bind(this)
+    }
+
+    componentDidMount() {
+        Axios.get('http://localhost:3001/disciplinas')
+            .then(
+                (response) => {
+                    this.setState({ disciplinas: response.data })
+                }
+            )
+            .catch(
+                (error) => {
+                    console.log(error)
+                }
+            )
+    }
+    
+    mountTable() {
+        if (!this.state.disciplinas) return
+        return this.state.disciplinas.map(
+            (disc, i) => {
+                return <TableRow disciplina={disc}
+                 key={i} 
+                /*apagarElementoPorId={this.apagarElementoPorId}*//>
+            }
+        )
+    }
+
     render(){
         return(
-            <div>
-                <p>Componente List</p>
+            <div style={{ marginTop: 20}}>
+                <h2>List Disciplinas</h2>
+                
+                <table className="table table-striped" style={{ marginTop: 20 }}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Curso</th>
+                            <th>Capacidade</th>
+                            <th colSpan="2" style={{ textAlign: "center" }}>Acões</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.mountTable()}
+                    </tbody>
+                </table>
             </div>
         )
     }
---
+++ atividade3/crud/src/components/TableRow.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+//import axios from 'axios'
+
+export default class TableRow extends Component {    
+    render() {
+        return (
+            <tr>
+                <td>
+                    {this.props.disciplina.id}
+                </td>
+                <td>
+                    {this.props.disciplina.nome}
+                </td>
+                <td>
+                    {this.props.disciplina.curso}
+                </td>
+                <td>
+                    {this.props.disciplina.capacidade}
+                </td>
+
+                <td style={{ textAlign: "center" }}>
+                    <Link to={"/edit/" + this.props.disciplina.id} className="btn btn-primary">Editar</Link>
+                </td>
+
+                <td style={{ textAlign: "center" }}>
+                    <button onClick={this.apagar} className="btn btn-danger">Deletar</button>
+                </td>
+            </tr>
+
+        )
+    }
+}
